ca084ed11e4f4c870488982501d281c7acd6f7a0
Yogesh <ytiwari01@gmail.com>
2026-04-03 10:42:28 +0530

    updated logo and constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-// For Vite (Common for Node 22+)
+// src/config.js or similar
 export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-// For Create React App
-// export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+// Note: In Production, VITE_API_URL will be "/api"
+// In Local, it will fall back to "http://localhost:5000"
